22844d7066e228f79942a54ab66c4c6eebedd17d
raf <sawickirafalit@gmail.com>
2025-02-16 14:43:12 +0100

    Chat-actions tests

--- BritneyAI/britneyai.client/src/app/components/chat/chat-actions/chat-actions.component.spec.ts
+++ BritneyAI/britneyai.client/src/app/components/chat/chat-actions/chat-actions.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatActionsComponent } from './chat-actions.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
 
 describe('ChatActionsComponent', () => {
   let component: ChatActionsComponent;
@@ -19,5 +20,37 @@ describe('ChatActionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit messageChange event when changed', () => {
+    spyOn(component.messageChange, 'emit');
+    const newMessage = 'New message';
+    component.messageChange.emit(newMessage);
+    expect(component.messageChange.emit).toHaveBeenCalledWith(newMessage);
+  });
+
+  it('should emit sendMessage event when triggered', () => {
+    spyOn(component.sendMessage, 'emit');
+    component.sendMessage.emit();
+    expect(component.sendMessage.emit).toHaveBeenCalled();
+  });
+
+  it('should emit cancelMessage event when triggered', () => {
+    spyOn(component.cancelMessage, 'emit');
+    component.cancelMessage.emit();
+    expect(component.cancelMessage.emit).toHaveBeenCalled();
+  });
+
+  it('should emit sendMessage event when click on send-button', () => {
+    spyOn(component.sendMessage, 'emit');
+
+    const sendButton = fixture.debugElement.query(
+      By.css('[testId="send-button"]')
+    );
+
+    sendButton.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(component.sendMessage.emit).toHaveBeenCalled();
   });
 });
